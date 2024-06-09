82df60cbf2b0bd296a6935d85af55ea512fe3b9f
RafalFDS2 <rafael.ferraz@picpay.com>
2024-06-08 23:42:32 -0300

    feat: initial template + channel's model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { HealthController } from './health/health.controller';
+import { localDbOptions, serverDbOptions } from './utils/option.connect';
+import { ChannelModule } from './channel/channel.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL, process.env.IS_LOCAL_DATABASE ? localDbOptions : serverDbOptions),
+    ChannelModule,
+  ],
+  controllers: [HealthController],
 })
 export class AppModule { }
